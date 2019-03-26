21abb082cbcfafd5ae0c353557b9ef7ea8651df8
Emil Kaidesoja <e.kaidesoja@gmail.com>
2019-03-26 15:38:32 +0200

    Spring application added

---
+++ MoodelInquiry/src/main/java/fi/haagahelia/MoodelInquiry/MoodelInquiryApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.MoodelInquiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodelInquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodelInquiryApplication.class, args);
+	}
+
+}
