6dcd5b797b148eb3777ec81d012b28aa326ec3e1
Marski96 <jani.maaranen@saunalahti.fi>
2019-03-31 18:11:04 +0300

    data shows in JSON

--- MoodelInquiry/src/main/java/fi/haagahelia/MoodelInquiry/MoodelInquiryApplication.java
+++ MoodelInquiry/src/main/java/fi/haagahelia/MoodelInquiry/MoodelInquiryApplication.java
@@ -1,13 +1,40 @@
 package fi.haagahelia.MoodelInquiry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.MoodelInquiry.domain.QuestionRepository;
+import fi.haagahelia.MoodelInquiry.domain.Question;
 
 @SpringBootApplication
 public class MoodelInquiryApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(MoodelInquiryApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MoodelInquiryApplication.class, args);
+	}
+	
+	@Bean 
+	public CommandLineRunner questionData(QuestionRepository QRepo) {
+		return (args)-> {
+			
+			log.info("save some questions");
+			QRepo.save(new Question(0, "1insert question here", "1insert answer here"));
+			QRepo.save(new Question(0, "2insert question here", "2insert answer here"));
+			
+			log.info("fetch questions");
+			for(Question question : QRepo.findAll()) {
+				log.info(question.toString());
+			}
+			
+			
+			
+		};
 	}
 
 }
---
+++ MoodelInquiry/src/main/java/fi/haagahelia/MoodelInquiry/domain/Question.java
@@ -0,0 +1,53 @@
+package fi.haagahelia.MoodelInquiry.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Question {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String questionText;
+	private String questionAnswer;
+	
+	public Question() {}
+	
+	public Question(long id, String questionText, String questionAnswer) {
+		
+		this.id = id;
+		this.questionText = questionText;
+		this.questionAnswer = questionAnswer;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getQuestionText() {
+		return questionText;
+	}
+
+	public void setQuestionText(String questionText) {
+		this.questionText = questionText;
+	}
+
+	public String getQuestionAnswer() {
+		return questionAnswer;
+	}
+
+	public void setQuestionAnswer(String questionAnswer) {
+		this.questionAnswer = questionAnswer;
+	}
+
+	@Override
+	public String toString() {
+		return "Question [id=" + id + ", questionText=" + questionText + ", questionAnswer=" + questionAnswer + "]";
+	}
+}
