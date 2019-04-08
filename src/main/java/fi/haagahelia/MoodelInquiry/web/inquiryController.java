60966c5e66263227f7403852780a77e7ea53a810
Emil Kaidesoja <e.kaidesoja@gmail.com>
2019-04-08 10:26:33 +0300

    allow cors

--- src/main/java/fi/haagahelia/MoodelInquiry/web/inquiryController.java
+++ src/main/java/fi/haagahelia/MoodelInquiry/web/inquiryController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import fi.haagahelia.MoodelInquiry.domain.*;
 
 @Controller
+@CrossOrigin
 public class inquiryController {
 	
 	@Autowired
