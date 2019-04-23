e2ec44e2828ec680becc2e0cff4e80b9112d8da4
elmeri <elmeri.kinnunen@myy.haaga-helia.fi>
2019-04-23 14:44:35 +0300

    CRUD controller basis to admin controller class

--- src/main/java/fi/haagahelia/MoodelInquiry/web/adminController.java
+++ src/main/java/fi/haagahelia/MoodelInquiry/web/adminController.java
@@ -3,9 +3,12 @@ package fi.haagahelia.MoodelInquiry.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import fi.haagahelia.MoodelInquiry.domain.AnswerRepository;
+import fi.haagahelia.MoodelInquiry.domain.Question;
 import fi.haagahelia.MoodelInquiry.domain.QuestionRepository;
 
 @Controller
@@ -22,6 +25,31 @@ public class adminController {
 		model.addAttribute("questions", qRepository.findAll());
 		return "questionList";
 	}
+	
+	@RequestMapping(value="/add")
+	public String add (Model model) {
+		model.addAttribute("question", new Question());
+		return "/addQuestion";
+	}
+	
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String save(Question question) {
+		qRepository.save(question);
+		return "redirect:questionList";
+	}
+	
+	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+	public String deleteQuestion(@PathVariable("id")Long id, Model model) {
+		qRepository.deleteById(id);
+		return "redirect:../questionList";
+	}
+	
+	@RequestMapping(value="/edit/{id}", method= RequestMethod.GET)
+	public String editQuestion(@PathVariable("id")Long id, Model model) {
+	model.addAttribute("question", qRepository.findById(id));
+	return "edit";
+}
+
 	@RequestMapping(value="/login")
 	public String login() {
 		return "login";
