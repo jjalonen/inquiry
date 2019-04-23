4c081d16aca3846020774b1c2f3e913b5326108c
Emil Kaidesoja <e.kaidesoja@gmail.com>
2019-04-23 14:09:12 +0300

    added login controller

--- src/main/java/fi/haagahelia/MoodelInquiry/web/adminController.java
+++ src/main/java/fi/haagahelia/MoodelInquiry/web/adminController.java
@@ -22,4 +22,8 @@ public class adminController {
 		model.addAttribute("questions", qRepository.findAll());
 		return "questionList";
 	}
+	@RequestMapping(value="/login")
+	public String login() {
+		return "login";
+	}
 }
