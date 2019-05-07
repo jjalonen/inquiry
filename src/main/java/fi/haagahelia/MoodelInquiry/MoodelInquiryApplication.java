5132b3df62f47c0806b628616ca512c5e931bf4c
elmeri <elmeri.kinnunen@myy.haaga-helia.fi>
2019-05-07 13:49:16 +0300

    create drob disabled

--- src/main/java/fi/haagahelia/MoodelInquiry/MoodelInquiryApplication.java
+++ src/main/java/fi/haagahelia/MoodelInquiry/MoodelInquiryApplication.java
@@ -22,55 +22,55 @@ public class MoodelInquiryApplication {
 	}
 
 	@Bean
-	public CommandLineRunner questionData(QuestionRepository QRepo, AnswerFormRepository ARepo, UserRepository urepository,
+	 public CommandLineRunner questionData(QuestionRepository QRepo, AnswerFormRepository ARepo, UserRepository urepository,
 			QuestionTypeRepository questionTypeRepository) {
 		return (args) -> {
-			log.info("save some questions and answers and questionType");
-			List<String> responseOptions = new ArrayList<>();
-			responseOptions.add("Finanssi- ja talousasiantuntiakoulutus tradenomi (AMK)");
-			responseOptions.add("Hotelli- ja ravintola-alan liikkeenjohdon koulutus restonomi (AMK)");
-			responseOptions.add("Johdon assistenttityön ja kielten koulutus tradenomi(AMK)");
-			responseOptions.add("Journalismikoulutus, medianomi(AMK)");
-			responseOptions.add("Liiketalouden koulutus tradenomi(AMK)");
-			responseOptions.add("Liikunnan ja vapaa-ajan koulutus liikunnanohjaaja(AMK)");
-			responseOptions.add("Matkailun koulutus restonomi(AMK)");
-			responseOptions.add("Matkailun liikkeenjohdon koulutus(AMK)");
-			responseOptions.add("Myynnin ja visuaalisen markkinoinnin koulutus tradenomi(AMK)");
-			responseOptions.add("Myyntityön koulutus tradenomi(AMK)");
-			responseOptions.add("Ruokatuotannon johtamisen koulutus restonomi(AMK)");
-			responseOptions.add("Tietojenkäsittelyn koulutus tradenomi (AMK)");
-			responseOptions.add("Englanninkieliset amk-tutkinnot");
-
-			questionTypeRepository.save(new QuestionType("Radio"));
-			questionTypeRepository.save(new QuestionType("Open Text"));
-			questionTypeRepository.save(new QuestionType("Select"));
-
-			List<String> question1ResponseOptions = new ArrayList<>();
-			question1ResponseOptions.add("1.vuoden");
-			question1ResponseOptions.add("2.vuoden");
-			question1ResponseOptions.add("3.vuoden");
-			question1ResponseOptions.add("4.vuoden");
-			question1ResponseOptions.add("5.vuoden");
-
-			QRepo.save(new Question(Long.valueOf(1), "Minkä vuoden opiskelija olet?",
-					questionTypeRepository.findByType("Radio").get(0), question1ResponseOptions));
-			QRepo.save(new Question(Long.valueOf(2), "Mikä on koulutusalasi?",
-					questionTypeRepository.findByType("Select").get(0), responseOptions));
-			QRepo.save(new Question(Long.valueOf(3), "Miten moodle toimii mielestäsi?",
-					questionTypeRepository.findByType("Open Text").get(0), null));
-
+//			log.info("save some questions and answers and questionType");
+//			List<String> responseOptions = new ArrayList<>();
+//			responseOptions.add("Finanssi- ja talousasiantuntiakoulutus tradenomi (AMK)");
+//			responseOptions.add("Hotelli- ja ravintola-alan liikkeenjohdon koulutus restonomi (AMK)");
+//			responseOptions.add("Johdon assistenttityön ja kielten koulutus tradenomi(AMK)");
+//			responseOptions.add("Journalismikoulutus, medianomi(AMK)");
+//			responseOptions.add("Liiketalouden koulutus tradenomi(AMK)");
+//			responseOptions.add("Liikunnan ja vapaa-ajan koulutus liikunnanohjaaja(AMK)");
+//			responseOptions.add("Matkailun koulutus restonomi(AMK)");
+//			responseOptions.add("Matkailun liikkeenjohdon koulutus(AMK)");
+//			responseOptions.add("Myynnin ja visuaalisen markkinoinnin koulutus tradenomi(AMK)");
+//			responseOptions.add("Myyntityön koulutus tradenomi(AMK)");
+//			responseOptions.add("Ruokatuotannon johtamisen koulutus restonomi(AMK)");
+//			responseOptions.add("Tietojenkäsittelyn koulutus tradenomi (AMK)");
+//			responseOptions.add("Englanninkieliset amk-tutkinnot");
+//
+//			questionTypeRepository.save(new QuestionType("Radio"));
+//			questionTypeRepository.save(new QuestionType("Open Text"));
+//			questionTypeRepository.save(new QuestionType("Select"));
+//
+//			List<String> question1ResponseOptions = new ArrayList<>();
+//			question1ResponseOptions.add("1.vuoden");
+//			question1ResponseOptions.add("2.vuoden");
+//			question1ResponseOptions.add("3.vuoden");
+//			question1ResponseOptions.add("4.vuoden");
+//			question1ResponseOptions.add("5.vuoden");
+//
+//			QRepo.save(new Question(Long.valueOf(1), "Minkä vuoden opiskelija olet?",
+//					questionTypeRepository.findByType("Radio").get(0), question1ResponseOptions));
+//			QRepo.save(new Question(Long.valueOf(2), "Mikä on koulutusalasi?",
+//					questionTypeRepository.findByType("Select").get(0), responseOptions));
+//			QRepo.save(new Question(Long.valueOf(3), "Miten moodle toimii mielestäsi?",
+//					questionTypeRepository.findByType("Open Text").get(0), null));
+//
 //			ARepo.save(new Answer("1. vuoden", Long.valueOf(1)));
 //			ARepo.save(new Answer("2. vuoden", Long.valueOf(1)));
 //			ARepo.save(new Answer("Tietojenkäsittelyn koulutus tradenomi (AMK)", Long.valueOf(2)));
-			
-			log.info("Saving some answers");
-			List<Answer> answers = new ArrayList<>();
-			answers.add(new Answer(Long.valueOf(1), "ihan ok"));
-			answers.add(new Answer(Long.valueOf(2), "hyvin huono"));
-
-			
-			
-			ARepo.save(new AnswerForm(answers));
+//			
+//			log.info("Saving some answers");
+//			List<Answer> answers = new ArrayList<>();
+//			answers.add(new Answer(Long.valueOf(1), "ihan ok"));
+//			answers.add(new Answer(Long.valueOf(2), "hyvin huono"));
+//
+//			
+//			
+//			ARepo.save(new AnswerForm(answers));
 //			ARepo.save(new AnswerForm(new Answer(Long.valueOf(1), "Aivan täydellinen"), new Answer(Long.valueOf(2), "Ihan okke")));
 
 			log.info("One user to rule them all");
